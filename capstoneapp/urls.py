fafab2545cd79a136fdec7d916ab00a7d3253a1e
henri <adokhenri@gmail.com>
2024-01-31 13:54:44 -0600

    Update urls in App Level with proper function in views.py for deleting Apis

--- capstoneapp/urls.py
+++ capstoneapp/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
 from capstoneapp import views
-from .views import capstoneapi_list, SaveFile, home_capstone_view
+from .views import capstoneapi_list, SaveFile, home_capstone_view, capstoneapi_detail
 
 urlpatterns = [
      # Home page URL
     path('', home_capstone_view, name='home'),
     #path('', views.index, name='index'),
     path('capstoneapi/', capstoneapi_list, name='capstoneapi-list'),
-    path('capstoneapi/<int:id>/', capstoneapi_list, name='capstoneapi-detail'),
+    path('capstoneapi/<int:id>/', capstoneapi_detail, name='capstoneapi-detail'),
 
      # File upload URL
     path('capstoneapi/SaveFile', SaveFile, name='save-file'),
--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -62,6 +62,15 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:8000'
 )
 
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
 ROOT_URLCONF = 'capstoneproject.urls'
 
 TEMPLATES = [
